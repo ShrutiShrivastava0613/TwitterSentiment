77bf3388623c27c47ff5907f46fa644bd46c88f8
ShrutiShrivastava0613 <109097615+ShrutiShrivastava0613@users.noreply.github.com>
2022-07-19 22:56:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import joblib 
+
+#load the joblib model 
+model_nb = joblib.load('Twitter Airline Sentiment')
+
+#user input 
+st.title("TWITTER AIRLINE SENTIMENT CLASSIFIER")
+ip = st.text_input("Enter your message:")
+
+#predict if the entered message is negative, positive or neutral 
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])  #prints the output as negative, positive or neutral
